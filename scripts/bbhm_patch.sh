aec1cb338321facbe2a452af0e12de1143a60c0c
mselva447 <murali_selvaraj3@comcast.com>
2021-03-09 19:26:47 +0000

    RDKB-34595: XB3 use (/nvram/syscfg.db), XB6 /opt/secure/data/syscfg.db
    Reason for change: For XB3:/nvram/syscfg.db XB6:/opt/secure/data/syscfg.db
    Test Procedure: 1) Able to get and set parameters from/to syscfg.db
    Risks: low
    Signed-off-by: murali selvaraj <murali_selvaraj3@comcast.com>
    
    (cherry picked from commit 7ed591a7845a05816a297d927399a4f6f87eb32f)
    Change-Id: Ibdf187d74950174d92cfdcf6e9697a7768fa67fc

--- scripts/bbhm_patch.sh
+++ scripts/bbhm_patch.sh
@@ -22,7 +22,7 @@
 #bbhm patch for 2.1s11
 
 . /etc/device.properties
-if [ "$BOX_TYPE" = "XB3" ] || [ "$BOX_TYPE" = "HUB4" ] || [ "$MODEL_NUM" = "TG4482A" ]; then
+if [ "$BOX_TYPE" = "XB3" ]; then
 SYSCFG_DB_FILE="/nvram/syscfg.db"
 else
 SYSCFG_DB_FILE="/opt/secure/data/syscfg.db"
